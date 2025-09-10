a2a4b34535f8cf2c4c995cce72fb370ad32253e8
Thabang <phoshokothabang1@gmail.com>
2025-09-10 20:07:50 +0200

    feat: define routes for each of the pages

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Services from './components/Services'
 import Contact from './components/Contact'
 
+
+
 function App() {
   
   return (
-    
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
