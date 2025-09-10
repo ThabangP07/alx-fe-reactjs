5c5ad83e625ba2c1630412fe6fbc94493e5457fa
Thabang <phoshokothabang1@gmail.com>
2025-09-10 18:01:04 +0200

    feat: use the imported modules on userProfile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -2,9 +2,10 @@ import React, { useContext } from "react";
 import UserContext from "./UserContext";
 
 const UserProfile = (props) => {
+  const userData = useContext(UserContext);
   return (
     <div>
-      <h2>{props.name}</h2>
+      <h2>{userData.name}</h2>
       <p>Age: {props.age}</p>
       <p>Bio: {props.bio}</p>
     </div>
