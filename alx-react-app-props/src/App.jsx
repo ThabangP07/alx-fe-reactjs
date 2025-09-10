8c9cba97e6ddb6caeea7cc365d2d642697c413f2
Thabang <phoshokothabang1@gmail.com>
2025-09-10 17:51:33 +0200

    refactor: move the context into the App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -4,18 +4,20 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import UserProfile from './components/UserProfile'
-import UserContext from './components/UserContext.jsx';
+import UserProfile from './components/UserProfile';
+import  UserContext from './UserContext';
 import ProfilePage from './components/ProfilePage';
 
 
 function App() {
 
-  return ( 
-    <UserContext>
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  
+  return (
+    <UserContext.Provider value={userData}>
       <ProfilePage />
-    </UserContext>
+    </UserContext.Provider>
   );
-}
+};
 
 export default App
