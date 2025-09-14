da8d68c8f850876fc08d07a9044c06fb73810d4d
Thabang <phoshokothabang1@gmail.com>
2025-09-14 18:28:13 +0200

    feat: add a functionality to search terms and compute filtered recipes based on the term

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,16 +3,25 @@ import { create } from "zustand";
 const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
-    set( (state) => ({ recipes: [...state.recipes, newRecipe] })),
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
-      recipes: state.recipes.map((recipe) =>  recipe.id === updatedRecipe.id ? updatedRecipe : recipe),
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
     })),
   deleteRecipe: (id) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    }))
+    })),
+  searchTerm: "",
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filteredRecipes: [],
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe => recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    )
+  })),
 }));
 
 export default useRecipeStore;
