6a68824a7453ab586c6b96659b245a960ae4ca2b
Thabang <phoshokothabang1@gmail.com>
2025-09-14 20:27:53 +0200

    style: add styling for the navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
+    <nav style={{ padding: '10px', backgroundColor: '#2d225eff' }
+    }>
+      <ul style={{ listStyle: 'none', display: 'flex', gap: '15px', margin: 0, padding: 0, justifyContent: 'center'
+       }}>
         <li>
           <Link to="/home">Home</Link>
         </li>
