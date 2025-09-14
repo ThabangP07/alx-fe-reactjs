1839709624d7ed82c97b51cfe9d25e5a10e2c8a1
Thabang <phoshokothabang1@gmail.com>
2025-09-14 15:27:21 +0200

    feat: add a delete recipe functionality to the zustand store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -9,6 +9,10 @@ const useRecipeStore = create((set) => ({
     set((state) => ({
       recipes: state.recipes.map((recipe) =>  recipe.id === updatedRecipe.id ? updatedRecipe : recipe),
     })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    }))
 }));
 
 export default useRecipeStore;
