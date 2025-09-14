6a1098ec061bb416beb54c81bfad1b51d5edc0ee
Thabang <phoshokothabang1@gmail.com>
2025-09-14 17:58:39 +0200

    refactor: implement react router on the App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
+import EditRecipeForm from "./components/EditRecipeForm";
 import AddRecipeForm from "./components/AddRecipeForm";
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipes" element={<RecipeList />} />
+          <Route path="/recipes/add" element={<AddRecipeForm />} />
+          <Route path="/recipes/edit/:id" element={<EditRecipeForm />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
