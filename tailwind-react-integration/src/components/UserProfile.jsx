8bf027110f3eb1bbcb2073cf8a81e4fd743c0af1
Thabang <phoshokothabang1@gmail.com>
2025-09-27 20:59:40 +0200

    style: add hover effect to the user name

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -8,7 +8,9 @@ function UserProfile() {
         src={UserImg}
         alt="User"
       />
-      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
+      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4 hover:text-red-500">
+        John Doe
+      </h1>
       <p className="text-gray-600 md:text-base sm:text-sm">
         Developer at Example Co. Loves to write code and explore new
         technologies.
