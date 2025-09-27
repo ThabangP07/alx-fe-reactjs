0c21f45f46c028e9200223e2ab9c343d36fd80e4
Thabang <phoshokothabang1@gmail.com>
2025-09-27 20:25:50 +0200

    refactor: import the image instead of using the relative path

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,9 @@
+import UserImg from '../resources/images/user.jpg';
+
 function UserProfile() {
   return (
     <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img className="rounded-full w-36 h-36 mx-auto" src="../resources/images/user.jpg" alt="User" />
+      <img className="rounded-full w-36 h-36 mx-auto" src={UserImg} alt="User" />
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-base">
         Developer at Example Co. Loves to write code and explore new
