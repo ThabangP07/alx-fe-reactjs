f8033dadc3cae739d4b25333167ccf140600e0ae
Thabang <phoshokothabang1@gmail.com>
2025-09-27 20:47:22 +0200

    style: add a hover effect on the user image

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,11 @@ import UserImg from '../resources/images/user.jpg';
 function UserProfile() {
   return (
     <div className="bg-gray-100 sm:p-4 md:p-8 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg">
-      <img className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto" src={UserImg} alt="User" />
+      <img
+        className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+        src={UserImg}
+        alt="User"
+      />
       <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 md:text-base sm:text-sm">
         Developer at Example Co. Loves to write code and explore new
