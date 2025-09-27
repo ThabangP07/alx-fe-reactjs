3545554ca9524a87db04764d1f461bffa1553c6a
Thabang <phoshokothabang1@gmail.com>
2025-09-27 20:35:49 +0200

    style: add typography responsiveness to the user profile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,8 +4,8 @@ function UserProfile() {
   return (
     <div className="bg-gray-100 sm:p-4 md:p-8 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg">
       <img className="rounded-full w-36 h-36 mx-auto" src={UserImg} alt="User" />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 md:text-base sm:text-sm">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
